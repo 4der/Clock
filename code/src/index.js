0f6a39f934f8feec578ca541231d40e6ecf2bf64
ader <adamsson.erika@gmail.com>
2018-04-03 15:07:52 +0200

    Erika Adamsson clock

--- code/src/index.js
+++ code/src/index.js
@@ -3,16 +3,55 @@ import ReactDOM from "react-dom"
 import "./index.css"
 
 class Clock extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { date: new Date() }
+  }
+
+  componentDidMount() {
+    this.startTime()
+  }
+
+  componentWillUnmount() {
+    this.freezeTime()
+  }
+
+  freezeTime = () => {
+    clearInterval(this.timerID)
+  }
+
+  startTime = () => {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    )
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    })
+  }
 
   render() {
     return (
-      <div>
+      <div className="container">
         <h1>Hello, world!</h1>
-        <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+        <h2>The time is: {this.state.date.toLocaleTimeString()} in Sweden right now.</h2>
+
+        <div className="buttons">
+          <button onClick={this.freezeTime}>
+        Stop Time
+          </button>
+
+          <button onClick={this.startTime}>
+        Start Time
+          </button>
+        </div>
       </div>
     )
   }
 
 }
 
-ReactDOM.render(<Clock date={new Date()} />, document.getElementById("root"))
+ReactDOM.render(<Clock />, document.getElementById("root"))
